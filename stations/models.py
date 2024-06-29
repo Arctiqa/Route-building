c9ceabc838b6dff0766790dc8688062b19fd5818
Arctiqa <Sinitro@yandex.ru>
2024-06-30 07:44:58 +0800

    update stations/models.py

--- stations/models.py
+++ stations/models.py
@@ -2,13 +2,17 @@ from django.utils import timezone
 
 from django.db import models
 
+from config import settings
+
 
 class Route(models.Model):
-    name = models.CharField(max_length=150, verbose_name='маршрут')
-    start_point = models.CharField(max_length=100, verbose_name='начальный пункт')
-    end_point = models.CharField(max_length=100, verbose_name='конечный пункт')
-    length = models.IntegerField(verbose_name='длина маршрута')
-    description = models.TextField(verbose_name='описание')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='пользователь')
+    start_point = models.CharField(max_length=255, verbose_name='координаты отправки')
+    end_point = models.CharField(max_length=255, verbose_name='координаты прибытия')
+    length = models.IntegerField(verbose_name='длина авто(метры)')
+    height = models.FloatField(verbose_name='высота от земли(метры)')
+    weight = models.FloatField(verbose_name='вес авто(тонны)')
+    axle_load = models.FloatField(verbose_name='нагрузка на ось(тонны)')
     date = models.DateTimeField(default=timezone.now, verbose_name='дата создания')
 
     class Meta:
@@ -17,11 +21,14 @@ class Route(models.Model):
 
 
 class GasStation(models.Model):
-    name = models.CharField(max_length=150, verbose_name='Название АЗС')
-    coordinates = models.CharField(max_length=50, verbose_name='Координаты')
-    address = models.TextField(verbose_name='Адрес')
-    diesel_price = models.DecimalField(max_digits=6, decimal_places=2, verbose_name='Цена дизельного топлива')
-    elevation = models.IntegerField(verbose_name='Высота над уровнем моря')
+    region = models.CharField(max_length=200, verbose_name='Регион', null=True, blank=True)
+    number = models.CharField(max_length=150, verbose_name='Номер АЗС', null=True, blank=True)
+    latitude = models.CharField(max_length=50, verbose_name='Широта', null=True, blank=True)
+    longitude = models.CharField(max_length=50, verbose_name='Долгота', null=True, blank=True)
+    related_service = models.TextField(verbose_name='Сопутствующий сервис', null=True, blank=True)
+    additional_service = models.TextField(verbose_name='Дополнительные услуги', null=True, blank=True)
+    diesel_price = models.CharField(max_length=50, verbose_name='Цена ДТ', null=True, blank=True)
+    taneko_diesel_price = models.CharField(max_length=50, verbose_name='Цена ДТ ТАНЕКО', null=True, blank=True)
     last_updated = models.DateTimeField(auto_now=True, verbose_name='Дата последнего обновления')
 
     class Meta:
@@ -32,9 +39,6 @@ class GasStation(models.Model):
 class Refueling(models.Model):
     route = models.ForeignKey(Route, on_delete=models.CASCADE, verbose_name='Маршрут')
     gas_station = models.ForeignKey(GasStation, on_delete=models.CASCADE, verbose_name='АЗС')
-    fuel_volume = models.DecimalField(max_digits=6, decimal_places=2, verbose_name='Объем топлива')
-    cost = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Стоимость')
-    refuel_date = models.DateTimeField(verbose_name='Дата и время заправки')
 
     class Meta:
         verbose_name = 'Заправка на маршруте'
