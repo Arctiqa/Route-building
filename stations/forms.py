3b347a58cb15f589b791fdaf579332793fda6265
Arctiqa <Sinitro@yandex.ru>
2024-07-01 00:40:53 +0800

    form

---
+++ stations/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from django.forms import ModelForm
+
+from stations.models import Route
+
+
+class MixinFormControl:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if field_name != 'is_published':
+                field.widget.attrs['class'] = 'form-control'
+
+
+class RouteForm(MixinFormControl, forms.ModelForm):
+    class Meta:
+        model = Route
+        fields = ('start_point', 'end_point', 'height', 'weight', 'axel_load')
+
