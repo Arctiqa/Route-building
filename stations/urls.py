d981b16da5433f554d28d6255acecbd3160f7e2c
Arctiqa <Sinitro@yandex.ru>
2024-07-01 00:40:44 +0800

    dependencies

--- stations/urls.py
+++ stations/urls.py
@@ -1,14 +1,17 @@
+from . import views
 from stations.apps import StationsConfig
 from django.urls import path
 
-from stations.views import IndexListView, RouteListView
+from stations.views import IndexListView, RouteView, RouteCreateView, RouteListView, RouteDetailView
 
 app_name = StationsConfig.name
 
 urlpatterns = [
     path('', IndexListView.as_view(), name='index'),
-    path('route_view', RouteListView.as_view(), name='route'),
-    path('route_list', RouteListView.as_view(), name='route-list'),
-
+    path('route_view/', RouteView.as_view(), name='route-view'),
+    path('route_create/', RouteCreateView.as_view(), name='create-route'),
+    path('route_list/', RouteListView.as_view(), name='route-list'),
+    path('stations/view/', views.gas_stations, name='gas-stations-view'),
+    path('route-detail/<int:pk>/', RouteDetailView.as_view(), name='route-detail'),
 
 ]
