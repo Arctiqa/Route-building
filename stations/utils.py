50b01024485abf98a56fd69460dd93fe349669da
Arctiqa <Sinitro@yandex.ru>
2024-07-02 22:09:27 +0800

    utils.py

--- stations/utils.py
+++ stations/utils.py
@@ -66,27 +66,27 @@ def get_tomtom_api_build_route(route):
         return None
 
 
-def get_nearest_stations(route_points, stations):
-    R = 6371.0
-
-    stations = stations.objects.all().value('latitude', 'longitude')
-
-    # Преобразование градусов в радианы
-    lat1 = math.radians(lat1)
-    lon1 = math.radians(lon1)
-    lat2 = math.radians(lat2)
-    lon2 = math.radians(lon2)
-
-    # Разницы широты и долготы
-    dlon = lon2 - lon1
-    dlat = lat2 - lat1
-
-    a = math.sin(dlat / 2) ** 2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2) ** 2
-    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
-
-    distance = R * c
-
-    return distance
+# def get_nearest_stations(route_points, stations):
+#     R = 6371.0
+#
+#     stations = stations.objects.all().value('latitude', 'longitude')
+#
+#     # Преобразование градусов в радианы
+#     lat1 = math.radians(lat1)
+#     lon1 = math.radians(lon1)
+#     lat2 = math.radians(lat2)
+#     lon2 = math.radians(lon2)
+#
+#     # Разницы широты и долготы
+#     dlon = lon2 - lon1
+#     dlat = lat2 - lat1
+#
+#     a = math.sin(dlat / 2) ** 2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2) ** 2
+#     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+#
+#     distance = R * c
+#
+#     return distance
 
 # def gas_stations_on_route(requests, pk):
 #     points = get_tomtom_api_build_route(requests, pk)
